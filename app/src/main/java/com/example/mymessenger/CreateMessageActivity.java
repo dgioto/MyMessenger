3a17c39eeb26f6e31d8e4055bfb665ab07ff38b0
Alexander Tih <dgioto@gmail.com>
2021-03-13 22:16:42 +0200

    отправляем сообщение во вторую активность

--- app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
@@ -20,19 +20,20 @@ public class CreateMessageActivity extends AppCompatActivity {
         EditText messageView = (EditText) findViewById(R.id.message);
         //привращаем сообщение в строку
         String  messageText = messageView.getText().toString();
-//        //в активнности this запускаем активность ReceiveMessageActiviyty
-//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-//        //вкладывем сообщение messageText  под ключем message  в интент
-//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        //в активнности this запускаем активность ReceiveMessageActiviyty
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //вкладывем сообщение messageText  под ключем message  в интент
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
 
-        //отправка интента в приложение по выбору, которое могут обрабатывать сообщения
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+//        //отправка интента в приложение по выбору, которое могут обрабатывать сообщения
+//        Intent intent = new Intent(Intent.ACTION_SEND);
+//        intent.setType("text/plain");
+//        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+//
+//        String chooserTitle = getString(R.string.chooser);
+//        Intent chooserIntent = Intent.createChooser(intent, chooserTitle);
+//        startActivity(chooserIntent);
 
-        String chooserTitle = getString(R.string.chooser);
-        Intent chooserIntent = Intent.createChooser(intent, chooserTitle);
-        startActivity(chooserIntent);
-//        startActivity(intent);
+        startActivity(intent);
     }
 }
